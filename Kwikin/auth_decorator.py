c01aff0846c45b43e69e6c3e80b8a4a4eb306eef
arturo <arturor19@gmail.com>
2020-06-20 13:48:23 -0500

    modificacion en el docorador para validar usuario

--- Kwikin/auth_decorator.py
+++ Kwikin/auth_decorator.py
@@ -1,7 +1,6 @@
 from flask import session, render_template
 from functools import wraps
 import sqlite3
-import json
 
 db_sqlite = 'kw.db'
 
@@ -47,7 +46,7 @@ def is_user(f):
         try:
             grupo = (db_execute(
                 "select id_grupo from asoc_usuario_grupo where id_usuario = (select id_usuario from usuarios "
-                "where email = '{}')".format("arturor19@gmail.com"))[0]['id_grupo'])
+                "where email = '{}')".format(session['profile']['email']))[0]['id_grupo'])
         except:
             grupo = None
         if grupo is not None:
